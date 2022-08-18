478bc4909865c912f3c06f4247050f4d45d17b31
wouteredeling@gmail.com <wouteredeling@gmail.com>
2022-08-18 12:41:08 +0200

    changes due to revision 2: added linear regression results

--- HIV/recompute_HIV_error.py
+++ HIV/recompute_HIV_error.py
@@ -1,11 +1,50 @@
+def draw():
+
+    plt.clf()
+    ax = fig.add_subplot(121)
+    ax.set_ylim(bottom=0.001)
+    ax.set_xlabel('training data size')
+    ax.set_ylabel('relative error e [%]')
+    ax.set_title('training error')
+    sns.despine(top=True)
+    offset=5
+    
+    # make the plot using all samples, not with confidence intervals
+    
+    ax.plot(data_size-offset, err_ANN_unconstrained[0:r+1,:,0].T * 100, '.', color='dodgerblue', label='unconstrained ANN')
+    ax.plot(data_size, err_ANN[0:r+1,:,0].T * 100, '.', color='mediumaquamarine', label='constrained ANN')
+    ax.plot(data_size+offset, err_DAS[0:r+1,:,0].T * 100, '.', color='salmon', label='DAS')
+    
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys())
+    
+    ax2 = fig.add_subplot(122, sharey=ax)
+    ax2.set_xlabel('training data size')
+    ax2.set_title('test error')
+
+    # make the plot using all samples, not with confidence intervals
+    ax2.plot(data_size, err_ANN[0:r+1,:,1].T * 100, '.', color='mediumaquamarine')
+    ax2.plot(data_size+offset, err_DAS[0:r+1,:,1].T * 100, '.', color='salmon')
+    ax2.plot(data_size-offset, err_ANN_unconstrained[0:r+1,:,1].T * 100, '.', color='dodgerblue')
+
+    sns.despine(left=True, ax=ax2)
+    ax2.get_yaxis().set_visible(False)
+    plt.tight_layout()
+    plt.pause(0.1)
 
 import numpy as np
 import matplotlib.pyplot as plt
 import easysurrogate as es
 import pandas as pd
+import seaborn as sns
+from collections import OrderedDict
 
 plt.close('all')
 plt.rcParams['image.cmap'] = 'seismic'
+
+fig = plt.figure(figsize=[8, 4])
+offset=5
 
 campaign = es.Campaign()
 
@@ -33,15 +72,20 @@ n_neurons = 100
 n_replicas = 100
 n_test_fracs = 10
 
-test_fracs = np.linspace(0.9, 0.1, n_test_fracs)
 err_ANN = np.zeros([n_replicas, n_test_fracs, 2])
 err_ANN_unconstrained = np.zeros([n_replicas, n_test_fracs, 2])
 err_DAS = np.zeros([n_replicas, n_test_fracs, 2])
 
+# test fractions
+test_fracs = np.linspace(0.9, 0.1, n_test_fracs)
+
+# size of training data used
+data_size = (1 - test_fracs) * samples.size
+
 ########################################
 # choose the active subspace dimension #
 ########################################
-d = 1
+d = 2
 
 for r in range(n_replicas):
 
@@ -110,6 +154,8 @@ for r in range(n_replicas):
         rel_err_train, rel_err_test = das_analysis.get_errors(params, samples, relative=True)
         err_DAS[r, n, 0] = rel_err_train
         err_DAS[r, n, 1] = rel_err_test
+        
+        draw()
 
 # store data
 campaign = es.Campaign()
